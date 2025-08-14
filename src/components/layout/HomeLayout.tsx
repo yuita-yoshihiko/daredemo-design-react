19cfe2cdd4b46459d6152ffeed970a405b4d7c0c
yuita-yoshihiko <soup998211@gmail.com>
2025-08-14 09:11:44 +0900

    フッターの反映

--- src/components/layout/HomeLayout.tsx
+++ src/components/layout/HomeLayout.tsx
@@ -1,15 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Header from '@/components/header/Header';
+import Footer from '@/components/footer/Footer';
 
 export default function HomeLayout() {
   return (
     <div className="min-h-dvh flex flex-col">
       <Header />
-      <main className="flex-1 container mx-auto px-4">
+      <main className="pb-6 sm:pb-8 lg:pb-12">
         {/* ここに各ページが差し込まれる */}
         <Outlet />
       </main>
-      {/* フッターを後で足すならここ */}
+      <Footer />
     </div>
   );
 }
