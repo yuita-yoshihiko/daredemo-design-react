818fccc482a2a98c5ef3de13b40feec8741abfb2
yuita-yoshihiko <soup998211@gmail.com>
2025-08-21 16:50:14 +0900

    トレンドページへのルーティング追加

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import HomeLayout from '@/components/layout/HomeLayout';
 import Home from '@/features/home/routes/Home';
+import Trend from '@/features/trend/routes/Trend';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route element={<HomeLayout />}>
-        <Route path="/hoge" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/trend" element={<Trend />} />
       </Route>
     </Routes>
   );
