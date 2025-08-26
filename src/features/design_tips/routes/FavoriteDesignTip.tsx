a5caddeffb0f5945345781c0906096d27341fdd9
yuita-yoshihiko <soup998211@gmail.com>
2025-08-26 19:13:42 +0900

    お気に入りページの作成

---
+++ src/features/design_tips/routes/FavoriteDesignTip.tsx
@@ -0,0 +1,13 @@
+const FavoriteDesignTip: React.FC = () => {
+  return <>
+    <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
+      <div className="sm:pb-8 lg:pb-12 mt-20">
+        <h1 className="font-serif text-gray-600 text-xl mb:text-3xl lg:text-4xl py-12">
+          お気に入り情報一覧
+        </h1>
+      </div>
+    </div>
+  </>;
+}
+
+export default FavoriteDesignTip;
