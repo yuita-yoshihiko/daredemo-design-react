ef9c91c6e09f51c47a48b0687a0fb5c3d2425505
yuita-yoshihiko <soup998211@gmail.com>
2025-09-08 15:23:47 +0900

    デザイン情報関連の型定義

---
+++ src/features/design_tips/types/index.ts
@@ -0,0 +1,27 @@
+export type DesignTip = {
+  title: string;
+  guidance: string;
+  url: string;
+  media: string;
+  categories: { name: string }[];
+};
+
+export type Category = {
+  name: string;
+};
+
+export type DesignTips = {
+  design_tips: DesignTip[];
+};
+
+export type DesignTipType = {
+  id: string;
+  attributes: {
+    title: string;
+    guidance: string;
+    url: string;
+    media: string;
+    categories: Category[];
+  };
+  type: 'design_tip';
+};
