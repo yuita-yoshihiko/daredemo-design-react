b23e636a8d5af41bcff931caf02bbea9e2d1317a
yuita-yoshihiko <soup998211@gmail.com>
2025-09-08 15:24:27 +0900

    デザイン情報画面

--- src/features/design_tips/routes/DesignTip.tsx
+++ src/features/design_tips/routes/DesignTip.tsx
@@ -1,4 +1,5 @@
 import { useMemo, useState } from "react";
+import { useQueryDesignTips } from "../hooks/useQueryDesignTips";
 
 const DesignTip: React.FC = () => {
   const fallbackTags = useMemo(
@@ -6,6 +7,8 @@ const DesignTip: React.FC = () => {
     []
   );
   const [activeTab, setActiveTab] = useState<0 | 1 | 2>(0);
+  const { data: designTips = [] } = useQueryDesignTips();
+
   return <>
     <div className="my-12 hidden bg-zinc-100 shadow xl:block">
       <hr />
@@ -38,6 +41,49 @@ const DesignTip: React.FC = () => {
           {label}
         </button>
       ))}
+    </div>
+    <div className="container mx-auto mt-8 md:mt-20 pb-16">
+      <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-8 md:gap-12 xl:gap-16">
+        {designTips
+          ?.filter((tip) => {
+            if (activeTab === 0) return tip.media === "Webサイト";
+            if (activeTab === 1) return tip.media === "書籍";
+            if (activeTab === 2) return tip.media === "動画";
+            return false;
+          })
+          .map((tip) => (
+            <div
+              key={tip.title}
+              className="my-12 bg-white flex shadow-lg flex-col rounded-lg border-2 border-green-700"
+            >
+              <div className="bg-green-700 py-2 text-center font-serif text-sm text-white">
+                {tip.media === "web"
+                  ? "Webサイト"
+                  : tip.media === "book"
+                  ? "書 籍"
+                  : "動 画"}
+              </div>
+              <div className="flex flex-col p-6 pt-8">
+                <div className="text-brand-brown text-xl font-serif hover:text-yellow-900 pt-4">
+                  {tip.title}
+                </div>
+                <div className="mx-auto mb-8 px-8 text-center text-gray-600">
+                  {tip.guidance}
+                </div>
+                <div className="mx-auto flex justify-center py-4">
+                  <a
+                    href={tip.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-sm font-serif font-light bg-green-700 hover:bg-green-600 text-white text-center rounded-lg"
+                  >
+                    詳細を見る
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   </>;
 }
